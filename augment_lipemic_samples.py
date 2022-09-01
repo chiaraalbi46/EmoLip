c71941c061dee4bc86665d3a78691b86238182f0
Chiara Albisani <chiaraalbi46@gmail.com>
2022-09-02 00:40:09 +0200

    Let augment_lipemic_samples.py be executable from terminal.

--- augment_lipemic_samples.py
+++ augment_lipemic_samples.py
@@ -65,65 +65,95 @@ def augment_image(image_array):
     return transform(image_array)
 
 
-datasetcsv = pd.read_csv('./dataset_files/open_new_copia.csv', names=['data', 'id', 'image', 'label'])
-label_group = datasetcsv.groupby(['label'])
-lip = label_group.get_group('lipemico')  # campioni lipemici
-id_lip = lip.groupby(['id'])
+if __name__ == '__main__':
 
-augmented_path = 'C:/Users/chiar/Desktop/Progetto_Bioimmagini/Campioniparticolari/augmented_samples/'
-# crea se non c'è
+    from dataset import write_csv_split
+    import argparse
 
-letters = ['p', 'q', 'r', 's', 't', 'u', 'v', 'z']  # to change the ids
+    parser = argparse.ArgumentParser(description="Create augmented lipemic samples")
 
-dcsv = datasetcsv
-for p in range(3):  # ciclo sul numero di volte in cui devo runnare il ciclo interno per aumentare i campioni lipemici
-    # le view lipemico sono 27 --> con 2 iterazioni arrivo a 54 e poi mi mancano 15 view da replicare
-    if p == 2:
-        en = enumerate(list(id_lip)[:15])
-    else:
-        en = enumerate(list(id_lip))
+    parser.add_argument("--augmented_folder", dest="augmented_folder",
+                        default="C:/Users/chiar/Desktop/Progetto_Bioimmagini/Campioniparticolari/augmented_samples/",
+                        help="path to folder to store the lipemic samples augmented")
+    parser.add_argument("--original_csv", dest="original_csv", default="./dataset_files/open_new.csv",
+                        help="path to the current (not augmented) dataset csv")
+    parser.add_argument("--augmented_csv", dest="augmented_csv", default="./dataset_files/augmented_dataset.csv",
+                        help="path to new augmented dataset csv")
+    parser.add_argument("--train_val_csv", dest="train_val_csv", default="./dataset_files/train_val_dataset_augmented_",
+                        help="path to new train/val split augmented dataset csv (path + start name for the file)")
 
-    for k, (id, view) in en:  # ciclo sui campioni lipemici da 'aumentare'
-        print("id: ", id)
-        images = view['image'].values.tolist()  # lista immagini per id
-        data = view['data'].values[0]
-        lab = view['label'].values[0]
-        block = []
-        for i in range(len(images)):
-            # applico data augmentation ad ogni immagine, la salvo e ricreo un dataframe da aggiungere a datasetcsv
-            im = images[i]
-            ra = read_image(im)
-            if ra is None:
-                new_im = augment_image(read_image(images[2]))
-            else:
-                new_im = augment_image(ra)
-            print("im name: ", im)
-            im_array = new_im.permute(1, 2, 0).numpy()  # h, w, c
-            # start_path = im.rpartition('Sample')[0]  # cartella che conterrà l'immagine
-            data_fold = im.rpartition('Sample_')[0].split('/')[-2]  # 28-feb-22
-            elements = im.rpartition('Sample_')[2].rpartition('Rotation')  # ('a0430', 'Rotation', '_00_Led_00.png')
-            id_new = elements[0] + letters[p]
-            # filename = start_path + id_new + elements[1] + elements[2]
-            if not os.path.exists(augmented_path + data_fold):
-                os.makedirs(augmented_path + data_fold)
-            filename = augmented_path + data_fold + '/' + 'Sample_' + id_new + elements[1] + elements[2]
+    parser.add_argument("--seed", dest="seed", default=42, help="seed for random train/validation split")
+    parser.add_argument("--val_perc", dest="val_perc", default=20, help="% for validation set")
 
-            plt.imsave(filename, im_array)
+    args = parser.parse_args()
 
-            row = [data, id_new, filename, lab]
-            block.append(row)
+    if not os.path.exists(args.augmented_folder):
+        os.makedirs(args.augmented_folder)
+
+    augmented_path = args.augmented_folder
+
+    datasetcsv = pd.read_csv(args.original_csv, names=['data', 'id', 'image', 'label'])
+    label_group = datasetcsv.groupby(['label'])
+    lip = label_group.get_group('lipemico')  # campioni lipemici
+    id_lip = lip.groupby(['id'])
+
+    letters = ['p', 'q', 'r', 's', 't', 'u', 'v', 'z']  # to change the ids
+
+    dcsv = datasetcsv
+    for p in range(3):  # ciclo sul numero di volte in cui devo runnare il ciclo interno per aumentare i campioni lipemici
+        # le view lipemico sono 27 --> con 2 iterazioni arrivo a 54 e poi mi mancano 15 view da replicare
+        if p == 2:
+            en = enumerate(list(id_lip)[:15])
+        else:
+            en = enumerate(list(id_lip))
+
+        for k, (id, view) in en:  # ciclo sui campioni lipemici da 'aumentare'
+            print("id: ", id)
+            images = view['image'].values.tolist()  # lista immagini per id
+            data = view['data'].values[0]
+            lab = view['label'].values[0]
+            block = []
+            for i in range(len(images)):
+                # applico data augmentation ad ogni immagine, la salvo e ricreo un dataframe da aggiungere a datasetcsv
+                im = images[i]
+                ra = read_image(im)
+                if ra is None:
+                    new_im = augment_image(read_image(images[2]))
+                else:
+                    new_im = augment_image(ra)
+                print("im name: ", im)
+                im_array = new_im.permute(1, 2, 0).numpy()  # h, w, c
+                # start_path = im.rpartition('Sample')[0]  # cartella che conterrà l'immagine
+                data_fold = im.rpartition('Sample_')[0].split('/')[-2]  # 28-feb-22
+                elements = im.rpartition('Sample_')[2].rpartition('Rotation')  # ('a0430', 'Rotation', '_00_Led_00.png')
+                id_new = elements[0] + letters[p]
+                # filename = start_path + id_new + elements[1] + elements[2]
+                if not os.path.exists(augmented_path + data_fold):
+                    os.makedirs(augmented_path + data_fold)
+                filename = augmented_path + data_fold + '/' + 'Sample_' + id_new + elements[1] + elements[2]
+
+                plt.imsave(filename, im_array)
+
+                row = [data, id_new, filename, lab]
+                block.append(row)
+                # break
+            block = pd.DataFrame(block, columns=datasetcsv.columns)
+            dcsv = pd.concat([dcsv, block], ignore_index=True)
             # break
-        block = pd.DataFrame(block, columns=datasetcsv.columns)
-        dcsv = pd.concat([dcsv, block], ignore_index=True)
-        # break
 
-# salva il nuovo dataframe su csv
-dcsv.to_csv('augmented_dataset.csv', index=False, header=False)
-# 1536 righe = 984 + (69*8)
-# 192 id = 123 + 69
+    # salva il nuovo dataframe su csv
+    dcsv.to_csv(args.augmented_csv, index=False, header=False)
+    # 1536 righe = 984 + (69*8)
+    # 192 id = 123 + 69
 
-# import numpy as np
-# ids = dcsv['id'].values  # all the id
-# _, idx = np.unique(ids, return_index=True)
-# unique_id = ids[np.sort(idx)]
+    # import numpy as np
+    # ids = dcsv['id'].values  # all the id
+    # _, idx = np.unique(ids, return_index=True)
+    # unique_id = ids[np.sort(idx)]
+
+    csv_in = args.augmented_csv  # 'augmented_dataset.csv'
+    csv_out = args.train_val + args.seed + '.csv'
+    write_csv_split(csv_in, csv_out, seed=int(args.seed), val_perc=int(args.val_perc))
+    new_csv = pd.read_csv(csv_out, names=['data', 'id', 'image', 'label', 'split'])
+
 
